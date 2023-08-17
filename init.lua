dc6b6aff9b4207cf5c31bbfc17e78cbacbe370cb
Essenly <64922022+Essenly@users.noreply.github.com>
2023-08-17 16:14:23 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,37 @@
-repeat task.wait() until game:IsLoaded()
-
 local Scripts = {
-    13995491879, -- Lisowo
+    [6938803436]  = {Name = "Anime Dimensions."},
 }
 
-if not table.find(Scripts, game.PlaceId) then return game.Players.LocalPlayer:Kick("Not supported place") end
+repeat task.wait() until game:IsLoaded()
 
-loadstring(game:HttpGet(`https://raw.githubusercontent.com/Essenly/NoName-Hub/main/{game.PlaceId}.lua`, true))()
+local PromptLib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Roblox/main/Useful/PromptLibrary.lua"))()
+local MarketplaceService = game:GetService("MarketplaceService")
+local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
+
+local Loaded = false
+
+local success, info = pcall(MarketplaceService.GetProductInfo, MarketplaceService, game.PlaceId)
+if not success then
+    PromptLib("Error", "Failed to retrieve product info. \n(Error Code: 117)", {
+        {Text = "OK", LayoutOrder = 0, Primary = true, Callback = function() end}
+    })
+    return
+end
+
+for i,v in pairs(Scripts) do
+    if game.PlaceId == i or string.match(info.Name, v.Name) then
+        print("Found supported game:", gameData.Name)
+        loadstring(game:HttpGet(`https://raw.githubusercontent.com/Essenly/NoName-Hub/main/{game.PlaceId}.lua`, true))()
+        Loaded = true
+    end
+end
+
+if Loaded == false then
+    return game.Players.LocalPlayer:Kick("NoName Hub does not support this game")
+end
+
+game.Players.LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.InProgress then
+        QueueOnTeleport("loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Essenly/NoName-Hub/main/init.lua')()")
+    end
+end)
