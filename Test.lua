7a9dd31b9582140ac22ee4be353df55c217a7931
Essenly <64922022+Essenly@users.noreply.github.com>
2024-01-27 19:46:32 +0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,10 +1,17 @@
 -- variables
 
+local HttpService = game:GetService("HttpService")
+
 local plr = game.Players.LocalPlayer
 local data = {
     AutoFishing = false,
-    AutoSpin = false,
+    
+    MacroName = "",
+    RecordMacro = false,
 }
+
+local timeout = tick()
+local MacroData = {}
 
 -- functions
 
@@ -50,6 +57,93 @@ function Spin()
     end)
 end
 
+function upgradeTower(id)
+    
+end
+
+function placeTower(id, position)
+    game:GetService("ReplicatedStorage"):WaitForChild("GenericModules"):WaitForChild("Service"):WaitForChild("Network"):WaitForChild("PlayerPlaceTower"):FireServer(id, position)
+end
+
+function saveMacroType(info)
+    if not data.RecordMacro then return end
+
+    info.Timeout = tick() - timeout
+
+    MacroData[#MacroData+1] = info
+
+    timeout = tick()
+end
+
+function saveMacro()
+    local macrodata = HttpService:JSONEncode(MacroData)
+    MacroData = {}
+
+    print(macrodata)
+end
+
+-- hookfunctions
+
+local placetower ; placetower = hookmetamethod(game:GetService("ReplicatedStorage"):WaitForChild("GenericModules"):WaitForChild("Service"):WaitForChild("Network"):WaitForChild("PlayerPlaceTower"), "__namecall", function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod():lower()
+
+    if not checkcaller() and method == "fireserver" then
+        saveMacroType({
+            Action = "PlaceTower",
+            Tower = args[1],
+            Position = args[2]
+        })
+    end
+
+    return placetower(self, ...)
+end)
+
+local upgradetower ; upgradetower = hookmetamethod(game:GetService("ReplicatedStorage"):WaitForChild("GenericModules"):WaitForChild("Service"):WaitForChild("Network"):WaitForChild("PlayerUpgradeTower"), "__namecall", function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod():lower()
+
+    if not checkcaller() and method == "fireserver" then
+        saveMacroType({
+            Action = "UpgradeTower",
+            Tower = args[1],
+        })
+    end
+
+    return upgradetower(self, ...)
+end)
+
+local set_target_tower ; set_target_tower = hookmetamethod(game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("GlobalInit"):WaitForChild("RemoteEvents"):WaitForChild("PlayerSetTowerTargetMode"), "__namecall", function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod():lower()
+
+    if not checkcaller() and method == "fireserver" then
+        saveMacroType({
+            Action = "SetTargetTower",
+            Tower = args[1],
+            Target = args[2]
+        })
+    end
+
+    return set_target_tower(self, ...)
+end)
+
+local sell_tower ; sell_tower = hookmetamethod(game:GetService("ReplicatedStorage"):WaitForChild("GenericModules"):WaitForChild("Service"):WaitForChild("Network"):WaitForChild("PlayerSellTower"), "__namecall", function(self, ...)
+    local args = {...}
+    local method = getnamecallmethod():lower()
+
+    if not checkcaller() and method == "fireserver" then
+        print("Sell Tower: "..args[1])
+
+        saveMacroType({
+            Action = "SellTower",
+            Tower = args[1],
+        })
+    end
+
+    return sell_tower(self, ...)
+end)
+
 -- coroutine
 
 coroutine.resume(coroutine.create(function()
@@ -72,6 +166,7 @@ Window:CreateHideButton(true)
 -- Tabs
 
 local Lobby = Window:CreateTab("Lobby")
+local Game = Window:CreateTab("Game")
 
 -- Lobby
 
@@ -84,11 +179,47 @@ Lobby:CreateToggle({
     end
 })
 
-Lobby:CreateToggle({
-    Name = "Spin Banner",
-    CurrentValue = false,
-    Flag = "Auto Spin",
+-- Game
+
+Game:CreateSection("Macro")
+
+Game:CreateBox({
+    Name = "Macro Name",
+    Placeholder = "Name here",
+    Flag = "MacroName",
     Callback = function(value)
-        data.AutoSpin = value
+        data.MacroName = value
     end
 })
+
+Game:CreateToggle({
+    Name = "Record Macro",
+    CurrentValue = false,
+    Flag = "RecordMacro",
+    Callback = function(value)
+        if string.len(data.MacroName) == 0 then
+            UI.Flags["RecordMacro"]:Set(false)
+            MacroData = {}
+            return
+        end
+
+        if value then
+            data.RecordMacro = true
+            MacroData = {}
+            timeout = tick()
+            return
+        end
+
+        data.RecordMacro = false
+        
+        if #MacroData > 0 then
+            saveMacro()
+        end
+    end
+})
+
+
+
+-- load
+
+UI:loadFlags()
