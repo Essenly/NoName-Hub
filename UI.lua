2bbb29571a96dc5ff7f52ca13288067fc2db4b12
Essenly <64922022+Essenly@users.noreply.github.com>
2024-01-29 03:10:04 +0300

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -275,6 +275,7 @@ function NoGui:Notify(data1, data2, data3)
 
 			if Position > 1 then
 				if not getgenv().Gui:FindFirstChild(Position - 1) then
+					Notify.Name = Position - 1
 					local decreaseTween = createTween(Notify, TweenInfo.new(0.3), {Position = UDim2.new(0.8, 0, Notify.Position.Y - 0.15, 0)})
 					decreaseTween:Play()
 					decreaseTween.Completed:Wait()
