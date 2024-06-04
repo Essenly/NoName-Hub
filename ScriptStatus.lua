deaa650e1c79995dcbf9e826861f224c7ffdb78c
Essenly <64922022+Essenly@users.noreply.github.com>
2024-06-04 12:58:17 +0300

    Update ScriptStatus.lua

--- ScriptStatus.lua
+++ ScriptStatus.lua
@@ -2,6 +2,6 @@ return {
 	["Anime Dimension"] = {
 		Status = "working",
 		Update = "04.06.2024",
-		Info = "Info for today: Nonentiy (moderator) will be best"
+		Info = "finally updated script"
 	}
 }
