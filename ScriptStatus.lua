515421ba47f00cfa140273c3f5c1c3f7cfe15462
Essenly <64922022+Essenly@users.noreply.github.com>
2024-07-28 15:29:09 +0300

    Update ScriptStatus.lua

--- ScriptStatus.lua
+++ ScriptStatus.lua
@@ -1,12 +1,12 @@
 return {
 	["Anime Dimension"] = {
 		Status = "Working",
-		Update = "18.07.2024",
+		Update = "25.07.2024",
 		Info = "no info"
 	},
 	["Gym League"] = {
 		Status = "Working",
-		Update = "16.07.2024",
+		Update = "25.07.2024",
 		Info = "no info"
 	},
 	["Void Fishing"] = {
